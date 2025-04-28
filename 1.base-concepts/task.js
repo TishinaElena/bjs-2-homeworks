b4e72e3950ca0bfc196955a25276d417c07df8d4
Elena Ovchinnikova <helen1ovc@gmail.com>
2025-04-28 13:03:08 +0300

    Решение задач по базовым концепциям JavaScript готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	const d = b ** 2 - 4 * a * c;
+	if (d < 0) {
+		arr = [];
+	} else if (d === 0) {
+		const x = -b / (2 * a);
+		arr = [x];
+	} else {
+		const x1 = (-b + Math.sqrt(d)) / (2 * a);
+		const x2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr = [x1, x2];
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	const p = (percent / 100) / 12;
+	const s = amount - contribution;
+	const n = countMonths;
+
+	if (contribution >= amount) {
+		return 0;
+	}
+
+	const monthlyPayment = s * (p + (p / ((1 + p) ** n - 1)));
+
+	const totalAmount = monthlyPayment * n;
+	return Number(totalAmount.toFixed(2));
 }
