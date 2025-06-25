3224a16028b0313a382e385052911f435f48dfd5
Elena Ovchinnikova <helen1ovc@gmail.com>
2025-06-25 13:35:17 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false; // Массивы разной длины не могут быть одинаковыми
+      }
+    
+      return arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0; // Или другое значение по умолчанию, если нет пользователей данного пола
+  }
+
+  const totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
+  const averageAge = totalAge / filteredUsers.length;
+
+  return averageAge;
 }
