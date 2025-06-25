60f664d661fbbb514e286a26b49580c853f9a258
Elena Ovchinnikova <helen1ovc@gmail.com>
2025-06-25 13:54:53 +0300

    Четвертое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+	this.subject = undefined;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		//console.log("Невозможно добавить оценки: студент отчислен.");
+		return;
+	}
+
+	for (const mark of marks) {
+		this.marks.push(mark);
+	}
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0; // Возвращаем 0, если нет оценок или студент отчислен
+	}
+
+	const sum = this.marks.reduce((total, mark) => total + mark, 0);
+	return sum / this.marks.length;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
